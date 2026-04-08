2e000a343ad139d2381c3982d06e87375fe64085
AMN-47 <akhilnagar57@gmail.com>
2026-04-08 17:07:10 +0200

    Created two variables, one for editing, and the other for the General Info Data (name, email and Phone

--- src/components/Generalinfo.jsx
+++ src/components/Generalinfo.jsx
@@ -1,5 +1,11 @@
 import { useState } from "react";
 
 function GeneralInfo() {
-    
+    const [isEditing, setIsEditing] = useState(true);
+
+    const [data, setData] = useState ({
+        name: "",
+        email: "",
+        phone: ""
+    });
 }
