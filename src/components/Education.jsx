d46db62d82b8f37c94fb50cbd27908e66ab94ac6
AMN-47 <akhilnagar57@gmail.com>
2026-04-09 20:11:09 +0200

    Added variables for education details

---
+++ src/components/Education.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function Education() {
+    const [isEditing, setIsEditing] = useState(true);
+
+    const [data, setData] = useState({
+        school: "",
+        study: "",
+        date: ""
+    });
+}
