27daa89116ec99e20fe07ccff8475588e26e07d4
AMN-47 <akhilnagar57@gmail.com>
2026-04-10 19:59:19 +0200

    Added useState data

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,5 +1,15 @@
 import { useState } from "react";
 
 function Experience() {
+    const [isEditing, setIsEditing] = useState(true);
+
+    const [data, setData] = useState({
+        company:"",
+        position: "",
+        responsibilities:"",
+        from:"",
+        to: ""
+    });
+
     
 }
