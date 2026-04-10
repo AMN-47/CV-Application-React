e07d78191b305efcbc0d122a90b126f44b8d638a
AMN-47 <akhilnagar57@gmail.com>
2026-04-10 06:26:58 +0200

    Added education in to main file'

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import GeneralInfo from "./components/Generalinfo";
-
+import Education from "./components/Education";
 
 function App() {
   return (
     <div>
       <h1>CV Builder</h1>
       <GeneralInfo/>
+      <Education/>
     </div>
   );
 }
